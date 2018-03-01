e4c2aa5cb86b44b1557038e29ae5730944cfb6e6
Maxim Lee <jetteim@gmail.com>
2018-03-01 18:00:06 +0300

    --

--- backend-app/app/controllers/markov_text_controller.rb
+++ backend-app/app/controllers/markov_text_controller.rb
@@ -1,2 +1,9 @@
 class MarkovTextController < ApplicationController
+	def show
+		render html: "<body></body>"
+	end
+
+	def generate
+		redirect_to :show
+	end
 end
--- backend-app/config/routes.rb
+++ backend-app/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/markov', to: 'markov_text#show'
+  post '/markov', to: 'markov_text#generate'
 end

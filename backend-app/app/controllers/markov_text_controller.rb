43eaf7fb1aa7588c8808ab2249df92ce83cd8f45
Maxim Lee <jetteim@gmail.com>
2018-03-02 02:04:55 +0300

    ---

--- backend-app/app/controllers/markov_text_controller.rb
+++ backend-app/app/controllers/markov_text_controller.rb
@@ -1,9 +1,21 @@
+FORM = "<h1>Генератор текста по цепям Маркова</h1>
+<form method='POST'>
+	<label for="keywords" >Ключевые слова или слоган/label>
+	<input type="text" name="keywords" id="keywords"><br>
+	<label for="dictionary" >Побольше текста/label><br>
+	<input type="textarea" name="dictionary" id="dictionary"/><br>
+	<input type="submit" value="Submit">
+</form>".html_safe
+
 class MarkovTextController < ApplicationController
 	def show
-		render html: "<h1>Генератор текста по цепям Маркова</h1>"
+		render html: FORM
 	end
 
 	def generate
-		redirect_to :show
+		p params[:keywords]
+		p params[:dictionary]
+		creative = params[:keywords] + params[:keywords]
+		render html: "<blockquote>#{creative}</blockquote>"
 	end
 end

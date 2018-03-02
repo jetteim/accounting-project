30f5431c9901a7db85a6ecb7eaf4c1fb43e63883
Maxim Lee <jetteim@gmail.com>
2018-03-02 04:50:20 +0300

    ---

--- backend-app/app/controllers/markov_text_controller.rb
+++ backend-app/app/controllers/markov_text_controller.rb
@@ -12,10 +12,10 @@ class MarkovTextController < ApplicationController
 
 	def creative
 		markov = MarkyMarkov::Dictionary.new('dictionary')
-		p markov.parse_string(markov.parse_string(params[:dictionary]).join)
+		p markov.parse_string(params[:dictionary]).join
 		res = []
-		res << "<h2>#{params[:sentences]} фразы:</h2><p>#{markov.generate_n_sentences params[:sentences]}</p>"
-		res << "<h2>фраза из #{params[:words]} слов:</h2><p>#{markov.generate_n_words params[:words]}</p>"
+		res << "<h2>#{params[:sentences]} фразы:</h2><p>#{markov.generate_n_sentences params[:sentences].to_i}</p>"
+		res << "<h2>фраза из #{params[:words]} слов:</h2><p>#{markov.generate_n_words params[:words].to_i}</p>"
 		res.join.html_safe
 	end
 
@@ -23,12 +23,12 @@ class MarkovTextController < ApplicationController
 		form = []
 		form << '<h1>Генератор текста по цепям Маркова</h1><form method="POST">'
 		form << '<label for="sentences">сгенерировать N фраз</label><br>'
-		form << '<input name="sentences" type="number" id="sentences">сгенерировать N фраз</label><br>'
+		form << '<input name="sentences" type="number" id="sentences"></input><br>'
 		form << '<label for="words">сгенерировать фразу из N слов</label><br>'
-		form << '<input name="words" type="number" id="words">сгенерировать N фраз</label><br>'
+		form << '<input name="words" type="number" id="words"></input><br>'
 		form << '<label for="dictionary" >Побольше текста</label><br>'
 		form << '<textarea rows="8" cols="80" type="textarea" name="dictionary" id="dictionary"></textarea> <br>'
-		form << '<form method="POST">   <input type="submit" value="Submit"></form>'
+		form << '<input type="submit" value="Submit"></form>'
 		@form = form.join
 	end
 

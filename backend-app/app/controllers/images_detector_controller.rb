8d42463aad027d3a3ee73271efe6091f4098ccc5
Maxim Lee <jetteim@gmail.com>
2018-03-08 00:58:39 +0300

    ---

---
+++ backend-app/app/controllers/images_detector_controller.rb
@@ -0,0 +1,61 @@
+require 'tensorflow'
+
+class ImagesDetectorController < ApplicationController
+  before_action :build_form
+
+  def show
+	render html: @form.html_safe
+  end
+
+  def submit
+  	p uploaded_io = params[:file]
+  	p @submitted = uploaded_io.read
+  	render html: "#{@form}<blockquote>#{classify(@submitted)}</blockquote>".html_safe
+  end
+
+  def classify (image = @submitted)
+	File.open(fname = Rails.root.join('public', 'tmp', SecureRandom.hex(10)), 'wb') do {|file|file.write(image)}
+
+	scope_class = Tensorflow::Scope.new
+  	input = Const(scope_class, fname)
+	output = input.operation.g.AddOperation(Tensorflow::OpSpec.new('ReadFile', 'ReadFile', nil, [input]))
+	output = input.operation.g.AddOperation(Tensorflow::OpSpec.new('DecodeJpeg', 'DecodeJpeg', Hash['channels' => 3], [output.output(0)]))
+	output = input.operation.g.AddOperation(Tensorflow::OpSpec.new('Cast', 'Cast', Hash['DstT' => 1], [output.output(0)]))
+	output = input.operation.g.AddOperation(Tensorflow::OpSpec.new('ExpandDims', 'ExpandDims', nil, [output.output(0), Const(scope_class.subscope('make_batch'), 0, :int32)]))
+	output = input.operation.g.AddOperation(Tensorflow::OpSpec.new('ResizeBilinear', 'ResizeBilinear', nil, [output.output(0), Const(scope_class.subscope('size'), [224, 224], :int32)]))
+	output = input.operation.g.AddOperation(Tensorflow::OpSpec.new('Sub', 'Sub', nil, [output.output(0), Const(scope_class.subscope('mean'), 117.00, :float)]))
+	output = input.operation.g.AddOperation(Tensorflow::OpSpec.new('Div', 'Div', nil, [output.output(0), Const(scope_class.subscope('scale'), 1.00, :float)])).output(0)
+	graph = scope_class.graph
+	session_op = Tensorflow::Session_options.new
+	session = Tensorflow::Session.new(graph, session_op)
+	out_tensor = session.run({}, [output], [])
+	graph = Tensorflow::Graph.new
+	graph.read_file(Rails.root.join('public', 'tmp', '/tensorflow_inception_graph.pb'))
+	tensor = Tensorflow::Tensor.new(out_tensor[0], :float)
+	sess = Tensorflow::Session.new(graph)
+	hash = {}
+	hash[graph.operation('input').output(0)] = tensor
+	predictions = sess.run(hash, [graph.operation('output').output(0)], [])
+	predictions.flatten!
+	labels = {}
+	j = 0
+	File.foreach('imagenet_comp_graph_label_strings.txt') do |line|
+	    labels[line] = predictions[j]
+	    j += 1
+	end
+	result = labels.sort { |a, b| b[1].to_f <=> a[1].to_f }
+	result.join '<br>'
+end
+
+
+	def build_form
+		form = []
+		form << '<h1>Анализ картинки</h1><form method="POST" enctype="application/x-www-form-urlencoded">'
+		form << '<label for="file">Картинка</label><br>'
+		form << '<input name="file" type="number" id="file"' + " value=\"#{params[:sentences]}\"" + '></input><br>'
+		form << '<input type="submit" value="Submit"></form>'
+		@form = form.join
+	end
+
+
+end
--- backend-app/config/routes.rb
+++ backend-app/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'images_detector/show'
+	post 'images_detector/submit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/markov', to: 'markov_text#show'
   post '/markov', to: 'markov_text#generate'

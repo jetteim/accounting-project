60cea9b27e3ce29419bc5916b68b121b3b0aa708
Maxim Lee <jetteim@gmail.com>
2018-03-08 05:10:36 +0300

    ---

--- backend-app/config/routes.rb
+++ backend-app/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'images', to: 'images_detector#show'
-  get 'images', to: 'images_detector#submit'
+  post 'images', to: 'images_detector#submit'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/markov', to: 'markov_text#show'

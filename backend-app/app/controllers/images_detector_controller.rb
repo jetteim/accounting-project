be46c5d12549c5d98ec2ef8a206b7920a70d9fe7
Maxim Lee <jetteim@gmail.com>
2018-03-18 05:09:25 +0300

    ---

--- backend-app/app/controllers/images_detector_controller.rb
+++ backend-app/app/controllers/images_detector_controller.rb
@@ -44,7 +44,7 @@ class ImagesDetectorController < ApplicationController
 	    j += 1
 	end
 	result = Hash[labels.sort { |a, b| b[1].to_f <=> a[1].to_f }]
-	result = result[0..params[:results]] if params[:results]
+	return params[:results] ? result : result[0..params[:results]]
   end
 
 
